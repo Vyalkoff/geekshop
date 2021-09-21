072fa3baea314621f5857349dff760cc957cb31f
vlad <kingbingping@gmail.com>
2021-09-21 19:01:24 +0500

    add products.json and done lesson_2

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,10 +1,20 @@
+from pathlib import Path
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    return render(request, 'products/index.html', context={'title': 'geekshop'})
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    PATH_APP_JS = Path(__file__).resolve().parent.joinpath('fixtu', 'products.json')
+    with open(PATH_APP_JS, 'r', encoding='utf-8') as f:
+        js_prod = json.load(f)
+    context = {
+        'title': 'geekshop',
+        'products': js_prod
+    }
+
+    return render(request, 'products/products.html', context=context)
