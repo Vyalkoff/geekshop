55a813470eea44cc72b38f297e6f130edbdda80c
vlad <kingbingping@gmail.com>
2021-09-24 19:25:13 +0500

    add migrate and change admin models

--- geekshop/products/models.py
+++ geekshop/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
