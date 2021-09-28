87e62b1fe3b885209795f969d9e9e94a503bc7aa
vlad <kingbingping@gmail.com>
2021-09-28 20:37:14 +0500

    add forms

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from users.forms import UserLoginForm
+
 
 def login(request):
+    form = UserLoginForm()
     context = {
         'title': 'Geekshop - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
 
 def register(request):
+
     context = {
         'title': 'Geekshop - Регистрация',
+
     }
     return render(request, 'users/register.html', context)
