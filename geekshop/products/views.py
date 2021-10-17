4abfe77537430fd3f960f0947f7a982913933d48
vlad <kingbingping@gmail.com>
2021-10-17 07:53:35 +0500

    paginator and show all products

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from .models import ProductCategory, Product
 
@@ -7,12 +8,20 @@ def index(request):
     return render(request, 'products/index.html', context={'title': 'geekshop'})
 
 
-def products(request):
-    context = {
-        'title': 'geekshop',
-        'products': Product.objects.all(),
-        'category_products': ProductCategory.objects.all(),
+def products(request, category_id=None, page_id=1):
+    product = Product.objects.filter(category_id=category_id) if category_id is not None else Product.objects.all()
+    paginator = Paginator(product, per_page=3)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
 
+    context = {
+        'title': 'Каталог',
+        'category_products': ProductCategory.objects.all(),
+        'products': products_paginator,
     }
 
     return render(request, 'products/products.html', context=context)
